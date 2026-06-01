623adcfb1a5333fe39df6f26fdd3f400118af42f
dilansachcha <dilansachintha44@gmail.com>
2026-06-02 02:31:40 +0530

    CORS dynamic origin mapping, and force DB table creation

--- src/main/java/lk/fortyfourss/ejb/bankingsystemee/rest/CorsFilter.java
+++ src/main/java/lk/fortyfourss/ejb/bankingsystemee/rest/CorsFilter.java
@@ -16,8 +16,13 @@ public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilt
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            String origin = requestContext.getHeaderString("Origin");
+            if (origin == null) {
+                origin = "*";
+            }
+
             requestContext.abortWith(Response.status(Response.Status.OK)
-                    .header("Access-Control-Allow-Origin", "http://localhost") // EXACT origin, not *
+                    .header("Access-Control-Allow-Origin", origin)
                     .header("Access-Control-Allow-Credentials", "true")
                     .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
                     .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
@@ -27,7 +32,12 @@ public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilt
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost");
+        String origin = requestContext.getHeaderString("Origin");
+        if (origin == null) {
+            origin = "*";
+        }
+
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
         responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
